5f69dcb3441b098b1fd2776d0fc04a9e383532f6
rgtalbot <rgtalbot@users.noreply.github.com>
2016-11-04 19:22:22 -0400

    change made to projects.js

--- app/models/projects.js
+++ app/models/projects.js
@@ -1,97 +1,127 @@
 var projects = [
-//     {
-//     name: "myportfolio",
-//     main: "MY PORTFOLIO",
-//     second: "",
-//     title: "My Personal Website",
-//     role: "ROLE: LEAD DEVELOPER"
-// },
     {
-    name: "rentaldashpro",
-    main: "RENTALDASH PRO",
-    second: "PROPERTY MANAGEMENT SYSTEM",
-    title: "RentalDash Pro - Property Management Dashboard",
-    role: "ROLE: JAVASCRIPT/FIREBASE DEVELOPER",
-    pageContent: [
-        // A web app that allows users to manage rental properties they own all in one convenient dashboard
-        // I worked on a team, and I was responsible for all of the CSS, use and implementation of parsely.js and chart.js, Firebase storage for user information, as well as assisting on JavaScript and jQuery writing.
-        "RentalDash Pro is a web app to help people manage rental properties they own all in one convenient dashboard.",
-        "I worked on a team and was mainly responsible for the UX/UI as well as Firebase for data storage. I used javascript libraries like parsely.js and chart.js to enhance the user experience with form validation and charts for displaying data. I also used Firebase to store user data and call it back with AJAX calls"
-    ],
-    pageLink: "https://www.rentaldashpro.com",
-    gitLink: "https://github.com/rgtalbot/rentaldashpro",
-    cardImg: "assets/img/portfolio/rentaldashpro/rdpcard.png",
-    images: ["assets/img/portfolio/rentaldashpro/rdpmain.png",
-        "assets/img/portfolio/rentaldashpro/rdpchart.png",
-        "assets/img/portfolio/rentaldashpro/rdpform.png"]
+        name: "mydirectory",
+        main: "MYDIRECTORY.IO",
+        second: "CONTACT MANAGEMENT SYSTEM",
+        title: "MyDirectory.io - Contact Management System",
+        role: "ROLE: WEB APP DEVELOPER",
+        pageContent: [
+            "one",
+            "tw0",
+            "three"
+        ],
+        pageLink: "http://mydirectory-io.herokuapp.com/",
+        gitLink: "https://github.com/ronnytomasetti/mydirectory",
+        cardImg: "",
+        images: [
+            "one",
+            "two"
+        ]
+    },
+    {
+        name: "rentaldashpro",
+        main: "RENTALDASH PRO",
+        second: "PROPERTY MANAGEMENT SYSTEM",
+        title: "RentalDash Pro - Property Management Dashboard",
+        role: "ROLE: JAVASCRIPT/FIREBASE DEVELOPER",
+        pageContent: [
+            "RentalDash Pro is a web app to help people manage rental properties they own all in one convenient dashboard.",
+            "I worked on a team and was mainly responsible for the UX/UI as well as Firebase for data storage. I used javascript libraries like parsely.js and chart.js to enhance the user experience with form validation and charts for displaying data. I also used Firebase to store user data and call it back with AJAX calls"
+        ],
+        pageLink: "https://www.rentaldashpro.com",
+        gitLink: "https://github.com/rgtalbot/rentaldashpro",
+        cardImg: "assets/img/portfolio/rentaldashpro/rdpcard.png",
+        images: [
+            "assets/img/portfolio/rentaldashpro/rdpmain.png",
+            "assets/img/portfolio/rentaldashpro/rdpchart.png",
+            "assets/img/portfolio/rentaldashpro/rdpform.png"
+        ]
 
-},{
-    name: "hangman",
-    main: "HANGMAN GAME",
-    second: "80's MOVIES STYLE",
-    title: "80s HANGMAN",
-    role: "ROLE: LEAD DESIGNER",
-    pageContent: [
-        "This is a hangman game I built using 80's movies as the theme. The logic is all done in javascript and I used jQuery for HTML manipulation. I also used Bootstrap CSS for the look. The game can be played with either the keyboard or by pushing buttons, allowing for a great user experience on a computer or mobile device",
-        "In addition to showing you the correct answer after you win or lose, a picture to go with that movie is displayed and a classic song from the movie begins to play. To stop the music and change the picture back, simply start playing again.",
-        "Feel free to play the game and let me know what you think of it!"
-    ],
-    pageLink: "https://eightieshangman.herokuapp.com/",
-    gitLink: "https://github.com/rgtalbot/week-3-game",
-    cardImg: "assets/img/portfolio/hangman/hangmancard.png",
-    images: ["assets/img/portfolio/hangman/hangman1.png"]
+    }, {
+        name: "hangman",
+        main: "HANGMAN GAME",
+        second: "80's MOVIES STYLE",
+        title: "80s HANGMAN",
+        role: "ROLE: LEAD DESIGNER",
+        pageContent: [
+            "This is a hangman game I built using 80's movies as the theme. The logic is all done in javascript and I used jQuery for HTML manipulation. I also used Bootstrap CSS for the look. The game can be played with either the keyboard or by pushing buttons, allowing for a great user experience on a computer or mobile device",
+            "In addition to showing you the correct answer after you win or lose, a picture to go with that movie is displayed and a classic song from the movie begins to play. To stop the music and change the picture back, simply start playing again.",
+            "Feel free to play the game and let me know what you think of it!"
+        ],
+        pageLink: "https://eightieshangman.herokuapp.com/",
+        gitLink: "https://github.com/rgtalbot/week-3-game",
+        cardImg: "assets/img/portfolio/hangman/hangmancard.png",
+        images: [
+            "assets/img/portfolio/hangman/hangman1.png"
+        ]
 
-}, {
-    name: "thrones",
-    main: "TRIVIA GAME",
-    second: "GAME OF THRONES STYLE",
-    title: "Game of Thrones Trivia",
-    role: "ROLE: LEAD DESIGNER",
-    pageContent: [
-        "This is a quick trivia game I built based on Game of Thrones. It uses javascript for logic as well as jQuery and Bootstrap CSS. The game logs correct and incorrect answers, displays the correct answer with a gif, and then after a timeout, moves on to the next question."
-        ,
-        "Once the game is over, the number of correct and incorrect answers are displayed along with the play again button. Playing again randomly draws 10 questions from the question bank so that you never get the same 10 questions in a row"
-        ,
+    }, {
+        name: "thrones",
+        main: "TRIVIA GAME",
+        second: "GAME OF THRONES STYLE",
+        title: "Game of Thrones Trivia",
+        role: "ROLE: LEAD DESIGNER",
+        pageContent: [
+            "This is a quick trivia game I built based on Game of Thrones. It uses javascript for logic as well as jQuery and Bootstrap CSS. The game logs correct and incorrect answers, displays the correct answer with a gif, and then after a timeout, moves on to the next question.",
+            "Once the game is over, the number of correct and incorrect answers are displayed along with the play again button. Playing again randomly draws 10 questions from the question bank so that you never get the same 10 questions in a row",
+            "Feel free to play the game and let me know what you think of it!"
 
-        "Feel free to play the game and let me know what you think of it!"
-
-    ],
-    pageLink: "https://thronestrivia.herokuapp.com/",
-    gitLink: "https://github.com/rgtalbot/TriviaGame",
-    cardImg: "assets/img/portfolio/portfolio2.png",
-    images: ["assets/img/portfolio/trivia/thrones1.png",
-        "assets/img/portfolio/trivia/thrones2.png",
-        "assets/img/portfolio/trivia/thrones3.png"]
-}, {
-    name: "template",
-    main: "Category",
-    second: "Project Name",
-    title: "PLACEHOLDER",
-    role: "ROLE: LEAD DESIGNER",
-    pageContent: [
-        "Lorem Impsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centures, but also the leap into electronic typesetting, remaining essentially unchanged."
-        ,
-        "Bacon ipsum dolor amet tri-tip short loin prosciutto, short ribs jerky rump landjaeger hamburger beef filet mignon. Tri-tip short ribs ground round landjaeger hamburger andouille turducken rump alcatra. Jerky porchetta flank, ham hock sausage landjaeger brisket. Strip steak shoulder prosciutto pork loin cupim, pork belly meatball. Cupim spare ribs andouille fatback. Andouille frankfurter jerky, cow biltong ribeye ground round short ribs. Hamburger pork belly sausage, landjaeger kevin leberkas tenderloin pig drumstick andouille alcatra pork chop jerky rump fatback."
-    ],
-    pageLink: "",
-    gitLink: "",
-    cardImg: "assets/img/portfolio/portfolio5.jpg",
-    images: ["assets/img/portfolio/template/pr01.jpg", "assets/img/portfolio/template/pr02.jpg"]
-}, {
-    name: "template",
-    main: "Category",
-    second: "Project Name",
-    title: "PLACEHOLDER",
-    role: "ROLE: LEAD DESIGNER",
-    pageContent: [
-        "Lorem Impsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centures, but also the leap into electronic typesetting, remaining essentially unchanged."
-        ,
-        "Bacon ipsum dolor amet tri-tip short loin prosciutto, short ribs jerky rump landjaeger hamburger beef filet mignon. Tri-tip short ribs ground round landjaeger hamburger andouille turducken rump alcatra. Jerky porchetta flank, ham hock sausage landjaeger brisket. Strip steak shoulder prosciutto pork loin cupim, pork belly meatball. Cupim spare ribs andouille fatback. Andouille frankfurter jerky, cow biltong ribeye ground round short ribs. Hamburger pork belly sausage, landjaeger kevin leberkas tenderloin pig drumstick andouille alcatra pork chop jerky rump fatback."
-    ],
-    pageLink: "",
-    gitLink: "",
-    cardImg: "assets/img/portfolio/portfolio5.jpg",
-    images: ["assets/img/portfolio/template/pr01.jpg", "assets/img/portfolio/template/pr02.jpg"]
-}];
+        ],
+        pageLink: "https://thronestrivia.herokuapp.com/",
+        gitLink: "https://github.com/rgtalbot/TriviaGame",
+        cardImg: "assets/img/portfolio/portfolio2.png",
+        images: [
+            "assets/img/portfolio/trivia/thrones1.png",
+            "assets/img/portfolio/trivia/thrones2.png",
+            "assets/img/portfolio/trivia/thrones3.png"
+        ]
+    }, {
+        name: "template",
+        main: "Category",
+        second: "Project Name",
+        title: "PLACEHOLDER",
+        role: "ROLE: LEAD DESIGNER",
+        pageContent: [
+            "Lorem Impsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centures, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "Bacon ipsum dolor amet tri-tip short loin prosciutto, short ribs jerky rump landjaeger hamburger beef filet mignon. Tri-tip short ribs ground round landjaeger hamburger andouille turducken rump alcatra. Jerky porchetta flank, ham hock sausage landjaeger brisket. Strip steak shoulder prosciutto pork loin cupim, pork belly meatball. Cupim spare ribs andouille fatback. Andouille frankfurter jerky, cow biltong ribeye ground round short ribs. Hamburger pork belly sausage, landjaeger kevin leberkas tenderloin pig drumstick andouille alcatra pork chop jerky rump fatback."
+        ],
+        pageLink: "",
+        gitLink: "",
+        cardImg: "assets/img/portfolio/portfolio5.jpg",
+        images: [
+            "assets/img/portfolio/template/pr01.jpg", "assets/img/portfolio/template/pr02.jpg"
+        ]
+    }, {
+        name: "template",
+        main: "Category",
+        second: "Project Name",
+        title: "PLACEHOLDER",
+        role: "ROLE: LEAD DESIGNER",
+        pageContent: [
+            "Lorem Impsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centures, but also the leap into electronic typesetting, remaining essentially unchanged.",
+            "Bacon ipsum dolor amet tri-tip short loin prosciutto, short ribs jerky rump landjaeger hamburger beef filet mignon. Tri-tip short ribs ground round landjaeger hamburger andouille turducken rump alcatra. Jerky porchetta flank, ham hock sausage landjaeger brisket. Strip steak shoulder prosciutto pork loin cupim, pork belly meatball. Cupim spare ribs andouille fatback. Andouille frankfurter jerky, cow biltong ribeye ground round short ribs. Hamburger pork belly sausage, landjaeger kevin leberkas tenderloin pig drumstick andouille alcatra pork chop jerky rump fatback."
+        ],
+        pageLink: "",
+        gitLink: "",
+        cardImg: "assets/img/portfolio/portfolio5.jpg",
+        images: [
+            "assets/img/portfolio/template/pr01.jpg", "assets/img/portfolio/template/pr02.jpg"
+        ]
+    }
+    // , {
+    //     name: "name",
+    //     main: "main",
+    //     second: "second",
+    //     title: "title",
+    //     role: "ROLE: role",
+    //     pageContent: ["one", "tw0", "three"],
+    //     pageLink: "",
+    //     gitLink: "",
+    //     cardImg: "",
+    //     images: ["one", "two"]
+    // }
+];
 
 module.exports = projects;
+
+
