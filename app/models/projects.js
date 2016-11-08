c12e0bb08fd97f022d39874fca631827abcaebfc
rgtalbot <rgtalbot@users.noreply.github.com>
2016-11-08 14:04:17 -0500

    updated projects

--- app/models/projects.js
+++ app/models/projects.js
@@ -37,11 +37,29 @@ var projects = [
             "assets/img/portfolio/mydirectory/mydirectory2.png"
         ]
     }, {
+        name: "eatdaburger",
+        main: "EAT DA BURGER",
+        second: "MySQL WEB APP",
+        title: "Eat Da Burger MVC web app with Node, Express, MySQL, and Handlebars",
+        role: "ROLE: WEB APP DEVELOPER",
+        pageContent: [
+            "Eat Da Burger is a web app that uses a MVC framework. It is built with Node, Express, MySQL, and Handlebars. The logic of the web app is simply to add, eat, and delete burgers from a MySQL database and that is displayed in the web app.",
+            "This was my first dive into using an ORM, in this case a homemade ORM, as well as my first dive into MVC frameworks and launching a MySQL database to the cloud. The project allowed me to grasp routing, models, and controllers so while the actual function of the app is a bit bland, it was a project I definitely learned a ton from."
+        ],
+        pageLink: "http://eat-da-burger-hw.herokuapp.com/burgers",
+        gitLink: "https://github.com/rgtalbot/eat-da-burger",
+        cardImg: "assets/img/portfolio/eatdaburger/eatdaburgercard.jpg",
+        images: [
+            "assets/img/portfolio/eatdaburger/eatdaburger1.png",
+            "assets/img/portfolio/eatdaburger/eatdaburger2.png",
+            "assets/img/portfolio/eatdaburger/eatdaburger3.png"
+        ]
+    }, {
         name: "hangman",
         main: "HANGMAN GAME",
         second: "80's MOVIES STYLE",
         title: "80s HANGMAN",
-        role: "ROLE: LEAD DESIGNER",
+        role: "ROLE: WEB APP DEVELOPER",
         pageContent: [
             "This is a hangman game I built using 80's movies as the theme. The logic is all done in javascript and I used jQuery for HTML manipulation. I also used Bootstrap CSS for the look. The game can be played with either the keyboard or by pushing buttons, allowing for a great user experience on a computer or mobile device",
             "In addition to showing you the correct answer after you win or lose, a picture to go with that movie is displayed and a classic song from the movie begins to play. To stop the music and change the picture back, simply start playing again.",
@@ -59,7 +77,7 @@ var projects = [
         main: "TRIVIA GAME",
         second: "GAME OF THRONES STYLE",
         title: "Game of Thrones Trivia",
-        role: "ROLE: LEAD DESIGNER",
+        role: "ROLE: WEB APP DEVELOPER",
         pageContent: [
             "This is a quick trivia game I built based on Game of Thrones. It uses javascript for logic as well as jQuery and Bootstrap CSS. The game logs correct and incorrect answers, displays the correct answer with a gif, and then after a timeout, moves on to the next question.",
             "Once the game is over, the number of correct and incorrect answers are displayed along with the play again button. Playing again randomly draws 10 questions from the question bank so that you never get the same 10 questions in a row",
@@ -73,22 +91,6 @@ var projects = [
             "assets/img/portfolio/trivia/thrones1.png",
             "assets/img/portfolio/trivia/thrones2.png",
             "assets/img/portfolio/trivia/thrones3.png"
-        ]
-    }, {
-        name: "template",
-        main: "Category",
-        second: "Project Name",
-        title: "PLACEHOLDER",
-        role: "ROLE: LEAD DESIGNER",
-        pageContent: [
-            "Lorem Impsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centures, but also the leap into electronic typesetting, remaining essentially unchanged.",
-            "Bacon ipsum dolor amet tri-tip short loin prosciutto, short ribs jerky rump landjaeger hamburger beef filet mignon. Tri-tip short ribs ground round landjaeger hamburger andouille turducken rump alcatra. Jerky porchetta flank, ham hock sausage landjaeger brisket. Strip steak shoulder prosciutto pork loin cupim, pork belly meatball. Cupim spare ribs andouille fatback. Andouille frankfurter jerky, cow biltong ribeye ground round short ribs. Hamburger pork belly sausage, landjaeger kevin leberkas tenderloin pig drumstick andouille alcatra pork chop jerky rump fatback."
-        ],
-        pageLink: "",
-        gitLink: "",
-        cardImg: "assets/img/portfolio/portfolio5.jpg",
-        images: [
-            "assets/img/portfolio/template/pr01.jpg", "assets/img/portfolio/template/pr02.jpg"
         ]
     }, {
         name: "template",
